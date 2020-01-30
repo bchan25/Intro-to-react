14cc3bba4833da4729b6b2c86519c2afe520d596
Ben Chan <ben@chanhq.com>
2020-01-30 11:01:31 +0000

    Added a paragraph
    Show you only return 1 element

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // What to show, Where to show it
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
