df495f0cbf004479295ae38172e6972c557abe51
Ben Chan <ben@chanhq.com>
2020-01-30 10:58:40 +0000

    Updated code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Require the REACT and REACTDOM modules
-var React = require("react");
-var ReactDOM = require("react-dom");
+import React from "react";
+import ReactDOM from "react-dom";
 
 // What to show, Where to show it
 ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
