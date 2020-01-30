bc166d0cf01b0a5772724791154c66ae5f7ed7e5
Ben Chan <ben@chanhq.com>
2020-01-30 11:19:24 +0000

    Template literals examples

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 // Example Data
-const name = "Ben";
+const fname = "Ben";
+const lname = "Chan";
 
 // What to show, Where to show it
 // {TEMPLATE LITERALS} EXPRESSIONS IS EQUAL TO A VALUE
 ReactDOM.render(
   <div>
-    <h1>Hello {name}!</h1>
+    <h1>
+      Hello {fname} {lname}!
+    </h1>
+    <h2>Goodbye {`${fname} ${lname}`}</h2>
     <p>Your lucky number is {3 + 4}</p>
   </div>,
   document.getElementById("root")
