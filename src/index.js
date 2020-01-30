67716566a8e813b254c0b6ae4b39decd4dd84747
Ben Chan <ben@chanhq.com>
2020-01-30 10:56:18 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// Require the REACT and REACTDOM modules
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+// What to show, Where to show it
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
